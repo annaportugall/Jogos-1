7397f3786485a97133df80e1451cdf1313c1e4e6
annaportugall <annaclaraazeredo@gmail.com>
2025-11-14 14:34:01 -0300

    lala

--- Assets/codigos/DeathZoneController.cs
+++ Assets/codigos/DeathZoneController.cs
@@ -1,16 +1,19 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DeathZoneController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if(other.CompareTag("Player")){
+            ReloadCurrentScene();
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    void ReloadCurrentScene()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
