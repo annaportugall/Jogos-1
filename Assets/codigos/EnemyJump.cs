0300482e70256e5d02d2e5404fb41a296301593a
annaportugall <annaclaraazeredo@gmail.com>
2025-11-28 14:13:42 -0300

    I-E-A-I-A-I-O

--- Assets/codigos/EnemyJump.cs
+++ Assets/codigos/EnemyJump.cs
@@ -5,6 +5,7 @@ public class EnemyJump : MonoBehaviour
   public float jumpForce;
 
   void OnTriggerEnter2D(Collider2D collision){
-    if(collision.gameObject.tag == "Enemy" ) collision.gameObject.sendMensage("jump", jumpForce);
+    if(collision.gameObject.tag == "Enemy" ) collision.gameObject.SendMessage("jump", jumpForce);
   }
 }
+    
